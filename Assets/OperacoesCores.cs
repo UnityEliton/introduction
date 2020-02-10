96d7ebaf00385dbe3effffe6f018387bf76d1bdf
Eliton Lab SENAC-SESC <elitonluiz93@gmail.com>
2020-02-10 18:32:48 -0300

    Tabela verdade AND ou E.

--- Assets/OperacoesCores.cs
+++ Assets/OperacoesCores.cs
@@ -6,20 +6,51 @@ using UnityEngine.UI;
 public class OperacoesCores : MonoBehaviour
 {
     public InputField inputEntrada;
+    public InputField inputEntradaX;
+    public InputField inputEntradaY;
     public Button buttonGo;
+    public Button buttonAnd;
+    public Button buttonOr;
     public Image imagem;
     public Image imagem2;
     // Start is called before the first frame update
     void Start()
     {
-        buttonGo.onClick.AddListener(operacoes);
+        //buttonGo.onClick.AddListener(operacoes);
+        buttonAnd.onClick.AddListener(tabelaAnd);
+        buttonOr.onClick.AddListener(tabelaOr);
+    }
+
+    void tabelaAnd()
+    {
+        bool varBoolX;
+        bool varBoolY;
+        if (inputEntradaX.text == "true")
+        {   varBoolX = true;  }
+        else{  varBoolX = false;  }
+        if (inputEntradaY.text == "true")
+        {   varBoolY = true;   }
+        else{  varBoolY = false;   }
+        if (varBoolX && varBoolY)
+        {
+            imagem.color = Color.green;
+            imagem2.color = Color.green;
+        }
+        else{
+            imagem.color = Color.red;
+            imagem2.color = Color.red;
+        }
+    }
+
+    void tabelaOr()
+    {
+
     }
 
     void operacoes()
     {
         float dadoDeEntrada = float.Parse(inputEntrada.text);
-        bool variavelBooleana;
-
+        
         if (dadoDeEntrada == 1)
         {
             imagem.color = new Color( 255, 255 , 255 , 100);
@@ -40,8 +71,8 @@ public class OperacoesCores : MonoBehaviour
         else if (dadoDeEntrada <= 3)
         {
             imagem2.color = Color.magenta;
-        }
-
+        } 
+        
     }
 
     // Update is called once per frame
