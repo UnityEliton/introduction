a1b49c638327e455734417fad8c0a8cb2234e294
Eliton Lab SENAC-SESC <elitonluiz93@gmail.com>
2020-02-17 16:18:00 -0400

    Fatorial com numeros inteiros.

--- Assets/Fatorial.cs
+++ Assets/Fatorial.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fatorial : MonoBehaviour
 {
+    public Button go;
+    public Text textResultado;
+    public InputField inputFat;
     // Start is called before the first frame update
     void Start()
     {
-        
+        go.onClick.AddListener(doFatorial);
+    }
+
+    void doFatorial()
+    {
+        int fatorial = int.Parse(inputFat.text);
+        int indice = 1;
+        int resultado = 1;
+        while(indice <= fatorial)
+        {
+            resultado = indice * resultado;
+            indice = indice + 1;
+        }
+        textResultado.text = resultado.ToString();
     }
 
     // Update is called once per frame
