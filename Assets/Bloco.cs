69517a3de813fa221395502933079f3b47b05eec
Eliton Lab SENAC-SESC <elitonluiz93@gmail.com>
2020-02-21 15:35:09 -0400

    Jogo do mosquito.

---
+++ Assets/Bloco.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bloco : MonoBehaviour
+{
+    public GameObject game;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+    //https://www.flaticon.com/free-icon/mosquito_1905068
+    void OnMouseDown()
+    {
+        print("Aqui");
+        game.GetComponent<GerenteGame>().listObj.Remove(this.gameObject);
+        // this object was clicked - do something
+        Destroy(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GerenteGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerenteGame : MonoBehaviour
+{
+    public List<GameObject> listObj = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void RemoveItem(GameObject im)
+    {
+        listObj.Remove(im);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
