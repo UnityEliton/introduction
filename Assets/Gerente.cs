5d5f9a1d5b1c0b87775cbf3940c030c3412960ce
Eliton Lab SENAC-SESC <elitonluiz93@gmail.com>
2020-02-27 17:15:03 -0400

    Mostrando o tempo final do jogo.

--- Assets/Gerente.cs
+++ Assets/Gerente.cs
@@ -10,6 +10,8 @@ public class Gerente : MonoBehaviour
     public int totalMosquitosMortos;
     public Text textPontos;
     public Text mensagemFinal;
+    public Text temporizador;
+    private float tempo = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,16 @@ public class Gerente : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        tempo = tempo + Time.deltaTime;
+        temporizador.text = ((int)tempo).ToString();
         textPontos.text = totalMosquitosMortos.ToString();
         if (listObjects.Count == 0)
         {
+            mensagemFinal.text = "Você ganhou!" +
+                "\ncom tempo: " + tempo.ToString();
             mensagemFinal.enabled = true;
             mensagemFinal.gameObject.SetActive(true);
+            Time.timeScale = 0;
         }
     }
 }
